08c976932a61ed29c5e8ccd4dfb2dfbd61329271
mionaD-upc <miona.dimic@estudiantat.upc.edu>
2023-04-11 11:52:11 +0200

    added DT classifier

--- run.sh
+++ run.sh
@@ -1,21 +1,24 @@
+
 #! /bin/bash
 
+#BASEDIR=/Users/miona.dimic/Desktop/TU
 BASEDIR=C:/Users/etiram/PycharmProjects/session2
+
 
 # convert datasets to feature vectors
 echo "Extracting features..."
-python extract-features.py $BASEDIR/data/train/ > train.feat
-python extract-features.py $BASEDIR/data/devel/ > devel.feat
+python3 extract-features.py $BASEDIR/data/train/ > train.feat
+python3 extract-features.py $BASEDIR/data/devel/ > devel.feat
 
 # train CRF model
 echo "Training CRF model..."
-python train-crf.py model.crf < train.feat
+python3 train-crf.py model.crf < train.feat
 # run CRF model
 echo "Running CRF model..."
-python predict.py model.crf < devel.feat > devel-CRF.out
+python3 predict.py model.crf < devel.feat > devel-CRF.out
 # evaluate CRF results
 echo "Evaluating CRF results..."
-python evaluator.py NER $BASEDIR/data/devel devel-CRF.out > devel-CRF.stats
+python3 evaluator.py NER $BASEDIR/data/devel devel-CRF.out > devel-CRF.stats
 
 
 #Extract Classification Features
@@ -24,13 +27,24 @@ cat train.feat | cut -f5- | grep -v ^$ > train.clf.feat
 
 # train Naive Bayes model
 echo "Training Naive Bayes model..."
-python train-sklearn.py model.joblib vectorizer.joblib < train.clf.feat
+python3 train-sklearn.py model.joblib vectorizer.joblib < train.clf.feat
 # run Naive Bayes model
 echo "Running Naive Bayes model..."
-python predict-sklearn.py model.joblib vectorizer.joblib < devel.feat > devel-NB.out
+python3 predict-sklearn.py model.joblib vectorizer.joblib < devel.feat > devel-NB.out
 # evaluate Naive Bayes results 
 echo "Evaluating Naive Bayes results..."
-python evaluator.py NER C:/Users/etiram/PycharmProjects/session2/data/devel devel-NB.out > devel-NB.stats
+python3 evaluator.py NER $BASEDIR/data/devel devel-NB.out > devel-NB.stats
+
+
+# train DT model
+echo "Training DT model..."
+python3 train-dt.py model.joblib vectorizer.joblib < train.clf.feat
+# run DT model
+echo "Running DT model..."
+python3 predict-sklearn.py model.joblib vectorizer.joblib < devel.feat > devel-DT.out
+# evaluate DT results 
+echo "Evaluating DT results..."
+python3 evaluator.py NER $BASEDIR/data/devel devel-DT.out > devel-DT.stats
 
 # remove auxiliary files.
 rm train.clf.feat
